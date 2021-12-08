efe97aab9790c179866236a0d90275d09a95e386
pawan kumar sharma <pawan1995219@gmail.com>
2021-12-08 23:38:43 +0530

    Initial commit

---
+++ operator.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a, b;
+    a = 40;
+    b = 0;
+    printf("a + b = %d\n", a|b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+    return 0;
+}
---
+++ wefewfwwe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter the value of no.\n");
+    scanf("%d",&a);
+    
+
+    return 0;
+}
